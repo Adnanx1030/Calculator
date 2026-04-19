67c71aff47bc1869c18f699fcc1c3402fb56c1a7
Adnan <adnanh1030@gmail.com>
2026-04-19 18:42:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-// 🔊 SOUND
-const sound = new Audio("assets/click.mp3");
+const display = document.getElementById("display");
 
-function playSound(){
-    sound.currentTime = 0;
-    sound.play();
-}
-
-// DISPLAY
-let display = document.getElementById("display");
-
-// FUNCTIONS
 function append(value){
     if(display.innerText === "0"){
         display.innerText = value;
@@ -17,19 +7,15 @@ function append(value){
         display.innerText += value;
     }
 }
-
 function clearDisplay(){
     display.innerText = "0";
 }
-
 function deleteLast(){
     display.innerText = display.innerText.slice(0, -1);
-
     if(display.innerText === ""){
         display.innerText = "0";
     }
 }
-
 function calculate(){
     try{
         display.innerText = eval(display.innerText);
